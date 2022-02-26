504e6d6aab40fa7d92542935c5a46575365d89bd
Alex Noble-James <alexnoble143.an@gmail.com>
2022-02-25 21:51:28 -0500

    began initial routing

---
+++ routes/api/notes.js
@@ -0,0 +1,5 @@
+const router = require('express').Router()
+
+//api get and post code
+
+module.exports = router
---
+++ server.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
+const PORT = process.env.PORT || 3001;
+const app = express();
+const apiRoutes = require('./routes/api')
+const htmlRoutes = require('./routes/html')
+const { notes } = require("./db/db.json");
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.use('/', htmlRoutes)
+app.use('/api', apiRoutes)
+
+
+app.listen(PORT, () => {
+  console.log(`App has started on port ${PORT}`);
+});
